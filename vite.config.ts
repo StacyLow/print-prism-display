32b0b103b075bf1da930ed2ad2d99c4196c3cbd5
Stacy Low <70250316+StacyLow@users.noreply.github.com>
2025-07-27 00:39:48 +1200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
-          }});
+          }}
         }
       }
     }
