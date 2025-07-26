caefc4641a575c3d8842ecaad15738aba87c3198
Stacy Low <70250316+StacyLow@users.noreply.github.com>
2025-07-27 01:21:20 +1200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import traceback
 
 app = Flask(__name__)
-CORS(app, origins=[r"http://localhost:\d+"])
+CORS(app, origins=["*"])
 
 def get_db_connection(config):
     """Create database connection from config"""
